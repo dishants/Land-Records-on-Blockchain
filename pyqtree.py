8ef59004e6543da6782b9f926c701f6be8c7c493
Dishant Shah <dishants@DS-Mac.local>
2016-08-02 15:11:45 +0100

    Added node comment

--- pyqtree.py
+++ pyqtree.py
@@ -447,6 +447,36 @@ def insertontoblockchain(data):
     issuehash=reply['txHex']
     print issuehash
 
+def sign():
+    """
+
+    NODEJS Code ->convert to python
+
+var bitcoin = require('bitcoinjs-lib');
+
+function signTx (unsignedTx, wif) {
+    var privateKey = bitcoin.ECKey.fromWIF(wif)
+    var tx = bitcoin.Transaction.fromHex(unsignedTx)
+    var insLength = tx.ins.length
+    console.log(insLength)
+    for (var i = 0; i < insLength; i++) {
+        tx.sign(i, privateKey)
+    }
+    return tx.toHex()
+}
+
+var key = 'L3fUpJPYjCAdNUiYy7auBXfcWGG5pJ8EqqNdxxdxVsGpS4PBjuWZ'
+// e.g. var key = 'KzH9zdXm95Xv3z7oNxzM6HqSPUiQbuyKoFdQBTf3HKx1B6eYdbAn';
+var txHex = '010000000192deb952db54bec2a20ce0055c1fa2dbd8fe603337617eeb40274829cb7821610100000000ffffffff020000000000000000086a064343020501107810f400000000001976a91457f0309b1fb4e184d4fa56a18f9aee0cca05f47f88ac00000000';
+// e.g. txHex = '0100000001e0cd69ce93aded7a8d51063ed5f7bb5c9cdcc885a93fa629574dedb2cd5b48ad0100000000ffffffff020000000000000000086a06434301050110b8820100000000001976a914ea55c2430dca31e56ef5ae55c2863dae65df908688ac00000000'
+
+var signedTxHex = signTx(txHex, key);
+console.log("signedTxHex: ["+signedTxHex+"]");
+
+
+
+    """
+
 def broadcast(hexdata):
     broadcasturl= 'http://testnet.api.coloredcoins.org:80/v3/broadcast'
     r=requests.post(broadcasturl,data={'txHex':hexdata})
